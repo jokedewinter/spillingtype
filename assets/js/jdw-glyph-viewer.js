6ecaf4c5fb6485726dcd7118ebb2fc2973f4671c
Joke De Winter <joke@jokedewinter.co.uk>
2024-06-05 18:22:20 +0100

    Adding glyph-viewer to all typefaces

---
+++ assets/js/jdw-glyph-viewer.js
@@ -0,0 +1,59 @@
+/*
+JavaScript for Viewing glyphs
+
+Author: Joke De Winter
+Author URI: https://www.jokedewinter.co.uk
+Version: 1.0
+*/
+
+
+
+// Use the json data to create the glyph grid
+function create_glyph_grid(data) {
+
+	var list = new Array();
+	
+	// Display the glyphs in category groups
+	for ( var i = 0; i < data.length; i++ ) {
+		list.push('<article>');
+		list.push('<h3 class="feature">' + data[i]['category'] + '</h3>');
+		var chars = data[i]['chars'];		
+		
+		for ( j = 0; j < chars.length; j++ ) {
+			const unicodeValue = chars[j];
+			/*
+			 * Use this if the json file has unicode values
+			 * using the UTF-8 format */
+			const character = String.fromCharCode(parseInt(unicodeValue, 16));		
+	
+			/*
+			 * Use this if the json file has either string values
+			 * or unicodes from string values */
+			//const character = unicodeValue; 
+			
+			if ( character ) {
+				list.push('<a id="' + character + '" href="#' + character + '" data-glyph="' + character + '">' + character + '</a>');
+			}
+		}
+		list.push('</article>');	
+	}
+	document.getElementById('glyphs').innerHTML = list.join('');
+//	window.scroll({top: 0, left: 0, behavior: 'smooth'});
+}
+
+
+// Choose a different font
+function chooseFont() {
+  	var chosenFont = document.getElementById("selectFont").value;
+	document.getElementById("glyphs").style.fontWeight = chosenFont;
+	document.getElementById("glyph").style.fontWeight = chosenFont;
+}
+
+
+// Display the chosen glyph
+function showGlyph(event) {
+	event.preventDefault(); 
+    var current = this.getAttribute('data-glyph');
+	var glyph = document.getElementById('glyph');
+	glyph.innerHTML = current;
+}
